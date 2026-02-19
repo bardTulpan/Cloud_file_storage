bc2b88c509a6a55cc31fba569a23213d4ef281a0
Bard <bard@example.com>
2026-02-20 01:15:48 +0800

    refactor: delete unuseful UserDetailsService

--- src/main/java/org/example/securitypractica/service/AuthService.java
+++ src/main/java/org/example/securitypractica/service/AuthService.java
@@ -8,14 +8,11 @@ import org.example.securitypractica.exception.InvalidCredentialsException;
 import org.example.securitypractica.mapper.UserMapper;
 import org.example.securitypractica.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AuthService implements UserDetailsService {
+public class AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
@@ -40,12 +37,5 @@ public class AuthService implements UserDetailsService {
         userRepository.save(user);
 
         return userMapper.toRegistrationResponseDto(user);
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
-        return new CustomUserDetails(user);
     }
 }
