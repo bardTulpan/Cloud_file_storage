170162ccd4605584a8a9c9c029281dc2498f5b3e
Bard <bard@example.com>
2026-02-20 12:46:27 +0800

    refactor: delete unuseful import

--- src/main/java/org/example/securitypractica/exception/StorageException.java
+++ src/main/java/org/example/securitypractica/exception/StorageException.java
@@ -1,7 +1,5 @@
 package org.example.securitypractica.exception;
 
-import io.minio.errors.ErrorResponseException;
-
 public class StorageException extends RuntimeException {
     public StorageException(String message, Exception e) {
         super(message);
